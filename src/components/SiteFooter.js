8d6ae784be695ee95f0d0ce79b0e3e13f78f3cbd
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-04 14:26:51 +0200

    Use provisional certification seal in footer

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -1,12 +1,22 @@
 import Link from 'next/link';
 
 export default function SiteFooter() {
+  const assetBase = process.env.GITHUB_PAGES === 'true' ? '/human-certification-web' : '';
+
   return (
     <footer className="siteFooter compactFooter">
       <div className="shell compactFooterMain">
-        <div className="compactFooterBrand">
-          <strong>MADE BY HUMANS</strong>
-          <span>Human-Made Product Standard</span>
+        <div className="compactFooterBrand compactFooterBrandWithSeal">
+          <img
+            className="footerSealTemporary"
+            src={`${assetBase}/brand/made-by-humans-seal.png`}
+            alt=""
+            aria-hidden="true"
+          />
+          <span className="compactFooterBrandText">
+            <strong>MADE BY HUMANS</strong>
+            <span>Human-Made Product Standard</span>
+          </span>
         </div>
 
         <nav className="compactFooterNav" aria-label="Footer Navigation">
