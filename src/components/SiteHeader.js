f16506c59dbc7e9faab2818e098ef570ea6c98ab
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-04 14:26:46 +0200

    Use provisional certification seal in header

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 
 export default function SiteHeader() {
+  const assetBase = process.env.GITHUB_PAGES === 'true' ? '/human-certification-web' : '';
+
   return (
     <header className="siteHeader shell">
-      <Link className="brandLockup" href="/" aria-label="Made by Humans Startseite">
-        <span className="brandName">MADE BY HUMANS</span>
-        <span className="brandDescriptor">Human-Made Product Standard</span>
+      <Link className="brandLockup brandLockupWithSeal" href="/" aria-label="Made by Humans Startseite">
+        <img
+          className="brandSealTemporary"
+          src={`${assetBase}/brand/made-by-humans-seal.png`}
+          alt=""
+          aria-hidden="true"
+        />
+        <span className="brandTextStack">
+          <span className="brandName">MADE BY HUMANS</span>
+          <span className="brandDescriptor">Human-Made Product Standard</span>
+        </span>
       </Link>
 
       <nav className="mainNav" aria-label="Hauptnavigation">
