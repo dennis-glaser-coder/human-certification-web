0586bd99d21cce3b1353e1b252a8314bf3782043
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-05 13:17:26 +0200

    Header klarer auf Verifizierung ausrichten

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -18,11 +18,11 @@ export default function SiteHeader() {
           <Link href="/register">Register</Link>
           <Link href="/dokumente">Dokumente</Link>
           <Link href="/ueber-uns">Über uns</Link>
-          <Link className="navVerify" href="/pruefen">ID prüfen</Link>
+          <Link className="navVerify" href="/pruefen">Zertifizierung prüfen</Link>
         </nav>
 
         <div className="mobileHeaderActions premiumMobileHeaderActions">
-          <Link className="mobileVerify" href="/pruefen">ID prüfen</Link>
+          <Link className="mobileVerify" href="/pruefen">Prüfen</Link>
           <MobileNav />
         </div>
       </div>
