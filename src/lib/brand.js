abc8c9fd50d584b95814f3a2505d4f353b67fb52
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-05 19:13:12 +0200

    Zentrale Marken- und Zertifizierungsaussagen definieren

---
+++ src/lib/brand.js
@@ -0,0 +1,3 @@
+export const BRAND_CLAIM = 'Von Menschen gemacht. Vor Ort geprüft.';
+export const CERTIFIED_STATEMENT = 'Dieses Produkt wurde in seinen wesentlichen Herstellungsschritten nachweislich durch Menschen gefertigt.';
+export const CERTIFICATION_SHORT = 'Geprüfte menschliche Herstellung – produktbezogen und öffentlich verifizierbar.';
