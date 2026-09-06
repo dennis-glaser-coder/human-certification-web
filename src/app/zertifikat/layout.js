4d2988ecbff182f493cd4f08e2704ab5575a69b6
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-06 09:03:58 +0200

    Prevent query-based certificate result pages from indexing

---
+++ src/app/zertifikat/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Öffentlicher Zertifizierungsdatensatz',
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
+
+export default function CertificateLookupLayout({ children }) {
+  return children;
+}
