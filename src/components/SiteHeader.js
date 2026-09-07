28234c7c0bb2880fdf39409767a1a7eb16615cd7
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-07 13:26:53 +0200

    Add organisation dropdown to desktop navigation

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -18,6 +18,17 @@ export default function SiteHeader() {
           <Link href="/fuer-kaeufer">Für Käufer</Link>
           <Link href="/register">Register</Link>
           <Link href="/standard">Standard</Link>
+
+          <details className="desktopNavDropdown">
+            <summary>Organisation</summary>
+            <div className="desktopNavDropdownPanel">
+              <Link href="/ueber-uns">Über uns</Link>
+              <Link href="/dokumente">Dokumente</Link>
+              <Link href="/transparenz">Transparenz & Integrität</Link>
+              <Link href="/verfahren">Beschwerden & Einsprüche</Link>
+            </div>
+          </details>
+
           <Link className="navVerify" href="/fuer-hersteller#zertifizierungsanfrage">Zertifizierung anfragen</Link>
         </nav>
 
