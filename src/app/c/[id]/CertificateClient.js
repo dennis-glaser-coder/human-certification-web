9a8375cd37390131c3ee5cfe546a68728720e900
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-07 21:42:43 +0200

    Use optimized full logo on certificate page

--- src/app/c/[id]/CertificateClient.js
+++ src/app/c/[id]/CertificateClient.js
@@ -126,7 +126,7 @@ export default function CertificateClient({ id }) {
               </div>
               <div className="certificateHeader">
                 <div className="certificateBrand">
-                  <img className="certificateMasterLogo" src={`${assetBase}/brand/made-by-human-logo.png`} alt="" aria-hidden="true" />
+                  <img className="certificateMasterLogo" src={`${assetBase}/brand/made-by-human-logo.webp`} alt="" aria-hidden="true" />
                   <div>
                     <small>ZERTIFIZIERUNGS-ID</small>
                     <h1>{state.record.public_id}</h1>
