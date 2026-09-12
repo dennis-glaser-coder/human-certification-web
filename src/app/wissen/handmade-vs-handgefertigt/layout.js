12845e2ca61e9fe86b6b39f93cfe224323ad2ac4
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-12 07:45:39 +0200

    Fix duplicated Handmade article title metadata

---
+++ src/app/wissen/handmade-vs-handgefertigt/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    default: 'Handmade oder handgefertigt? | Made by Human',
+    template: '%s',
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
