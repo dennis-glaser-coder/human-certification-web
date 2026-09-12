72337b4e6eaaa6d508f15c62e2f8e095a6074a95
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-12 07:45:11 +0200

    Fix duplicated SEO title for Handarbeit certification

---
+++ src/app/handarbeit-zertifizieren/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    default: 'Handarbeit zertifizieren lassen | Made by Human',
+    template: '%s',
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
