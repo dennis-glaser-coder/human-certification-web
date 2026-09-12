7097200c8555d3ce88a950347b35269d7d65be09
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-12 07:45:24 +0200

    Fix duplicated article title metadata

---
+++ src/app/wissen/was-bedeutet-handgemacht/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    default: 'Was bedeutet handgemacht? | Made by Human',
+    template: '%s',
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
