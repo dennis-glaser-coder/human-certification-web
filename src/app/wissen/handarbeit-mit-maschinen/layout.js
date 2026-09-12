de09afdc107a9f3bc2226f36f9d7a84a796bec1d
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-12 07:45:30 +0200

    Fix duplicated machine article title metadata

---
+++ src/app/wissen/handarbeit-mit-maschinen/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    default: 'Handarbeit mit Maschinen – geht das? | Made by Human',
+    template: '%s',
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
