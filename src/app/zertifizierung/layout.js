2477a917e3340fd445967e54c0fc51d841872ce9
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-09-12 07:45:18 +0200

    Fix duplicated SEO titles for product certifications

---
+++ src/app/zertifizierung/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    default: 'Zertifizierung für handgemachte Produkte | Made by Human',
+    template: '%s',
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
